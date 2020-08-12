b5a901a5297f4e9769d7f50f72c565913ca938dd
Prince Asiedu <princekofasiedu@gmail.com>
2020-08-12 15:32:05 -0700

    Handled user api errors

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+
 const uri = process.env.db_URI;
 
 const db = mongoose.connection;
@@ -24,23 +26,24 @@ app.get("/", (req, res) => {
 const historic = require("./routes/words");
 const users = require("./routes/users");
 
+
+app.use("/api/history", historic);
+app.use("/api/users", users);
+
 app.use((req, res, next) => {
   const error = new Error("Invalid");
   error.status = 404;
   next(error);
 });
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
   res.json({
     error: {
-      message: err.message,
+      message: error.message,
     },
   });
   next();
 });
-
-app.use("/api/history", historic);
-app.use("/api/users", users);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
